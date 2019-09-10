3ef3e7e86368f599ee466fea99b4c833c3c5fd28
Suwen sp Ubuntu <577014284@qq.com>
2019-09-10 09:52:31 +0800

    rty fixed

--- Student/app/src/main/java/com/example/student/MainActivity.java
+++ Student/app/src/main/java/com/example/student/MainActivity.java
@@ -20,32 +20,32 @@ public class MainActivity extends AppCompatActivity {
     Button btn_add;
     Button btn_send;
     ArrayList<Student>students=new ArrayList<Student>();
-    ArrayList<Student>studentlist=new ArrayList<Student>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn_send=findViewById(R.id.btn_send);
         btn_add=findViewById(R.id.btn_add);
+        et_name=findViewById(R.id.et_name);
+        et_major=findViewById(R.id.et_major);
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent();
                 intent.setClass(MainActivity.this, OtherActivity.class);
-                intent.putExtra("studnets", (Serializable) students);
+                intent.putExtra("stu", (Serializable) students);
                 startActivity(intent);
             }
         });
         btn_add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                add(view);
+                addS(view);
             }
         });
     }
-    public void add(View view){
-        et_name=findViewById(R.id.et_name);
-        et_major=findViewById(R.id.et_major);
+    public void addS(View view){
+
         String major=et_major.getText().toString();
         String name=et_name.getText().toString();
         Student student=new Student(name,major);
--- Student/app/src/main/java/com/example/student/OtherActivity.java
+++ Student/app/src/main/java/com/example/student/OtherActivity.java
@@ -11,12 +11,14 @@ public class OtherActivity extends AppCompatActivity {
     TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
-        tv=findViewById(R.id.tv_1);
+        tv=(TextView) findViewById(R.id.tv_1);
         Bundle bundle=getIntent().getExtras();
-        ArrayList<Student>students=(ArrayList<Student>)bundle.get("students");
-        for(Student s:students){
+        ArrayList<Student>students=(ArrayList<Student>)bundle.get("stu");
+        for(Student s:students)
+        {
             String stuInfo="姓名："+s.getName()+"， 专业："+s.getMajor()+"\n";
             tv.append(stuInfo);
         }
