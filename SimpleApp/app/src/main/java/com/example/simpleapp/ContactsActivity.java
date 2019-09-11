d925b31005a755c1dd3c34e7c7629fc55daa8db6
Suwen sp Ubuntu <577014284@qq.com>
2019-09-11 15:09:11 +0800

    second task set

---
+++ SimpleApp/app/src/main/java/com/example/simpleapp/ContactsActivity.java
@@ -0,0 +1,13 @@
+package com.example.simpleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ContactsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
+    }
+}
