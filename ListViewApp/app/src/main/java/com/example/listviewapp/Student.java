d981a4b8173632e4b38814e3b25f04895ade2601
Suwen sp Ubuntu <577014284@qq.com>
2019-09-25 11:27:38 +0800

    list e.g. 2

---
+++ ListViewApp/app/src/main/java/com/example/listviewapp/Student.java
@@ -0,0 +1,35 @@
+package com.example.listviewapp;
+
+import java.io.Serializable;
+
+public class Student implements Serializable {
+    Student() {
+        this.name = "Name";
+        this.major = "Major";
+    }
+
+    Student(String name, String major) {
+        this.name = name;
+        this.major = major;
+    }
+
+    private String name;
+    private String major;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+}
+
