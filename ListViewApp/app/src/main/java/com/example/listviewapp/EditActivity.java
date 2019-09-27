b70d0d14aa4d335759cd6db0a120cde01439e810
Suwen Hasee Ubuntu <577014284@qq.com>
2019-09-27 19:56:01 +0800

    finish college sex

--- ListViewApp/app/src/main/java/com/example/listviewapp/EditActivity.java
+++ ListViewApp/app/src/main/java/com/example/listviewapp/EditActivity.java
@@ -7,8 +7,12 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
 import android.widget.Toast;
@@ -17,48 +21,91 @@ import static android.widget.Toast.LENGTH_LONG;
 
 public class EditActivity extends AppCompatActivity {
     EditText et_name;
-    Spinner sp_major;
+    Spinner sp_college;
     EditText et_age;
-    Spinner sp_sex;
     Button btn_edit;
+    RadioGroup mRadioGroupSex;
+    RadioButton mRadioButtonMan;
+    RadioButton mRadioButtonWoman;
+    AutoCompleteTextView auto_major;
+    String sex = "男";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+
+        String[] major_array = {"物联网工程",
+                "计算机科学与技术",
+                "材料科学与工程",
+                "环境科学与工程",
+                "化学化工与生物工程"};
+        auto_major = findViewById(R.id.edit_major);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                this,
+                android.R.layout.simple_dropdown_item_1line,
+                major_array);
+        //初始化autoCompleteTextView
+        auto_major.setAdapter(adapter);
+        //设置输入多少字符后提示，默认值为2，在此设为１
+        auto_major.setThreshold(1);
+
+        mRadioGroupSex = findViewById(R.id.rg_sex);
+        mRadioButtonMan = findViewById(R.id.rb_man);
+        mRadioButtonWoman = findViewById(R.id.rb_woman);
+        mRadioGroupSex.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup rg, int checkedId) {
+                if (checkedId == mRadioButtonMan.getId()) {
+                    sex = "男";
+                } else if (checkedId == mRadioButtonWoman.getId()) {
+                    sex = "女";
+                } else {
+                    sex = "男";
+                }
+            }
+        });
         Intent intent = getIntent();
         Bundle bundle = intent.getExtras();
         Student stu = (Student) bundle.get("stu");
+        auto_major.setText(stu.getMajor());
+
         final int position = (int) bundle.get("position");
-        setContentView(R.layout.activity_edit);
+        //默认选中
+        if (stu.getSex() == "男")
+            ((RadioButton) mRadioGroupSex.getChildAt(0)).setChecked(true);
+        else
+            ((RadioButton) mRadioGroupSex.getChildAt(1)).setChecked(true);
+
         et_name = findViewById(R.id.edit_name);
-        sp_major = findViewById(R.id.edit_major);
+        sp_college = findViewById(R.id.edit_college);
         et_age = findViewById(R.id.edit_age);
         et_age.setInputType(EditorInfo.TYPE_CLASS_PHONE);
-        sp_sex = findViewById(R.id.edit_sex);
         et_name.setText(stu.getName());
-        setSpinnerDefaultValue(sp_major, stu.getMajor());
+        setSpinnerDefaultValue(sp_college, stu.getCollege());
         et_age.setText(String.valueOf(stu.getAge()));
-        setSpinnerDefaultValue(sp_sex, stu.getSex());
         btn_edit = findViewById(R.id.btn_edit);
         btn_edit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
-                int age=0;
+                int age = 0;
                 String name = et_name.getText().toString();
-                String major = sp_major.getSelectedItem().toString();
+                String college = sp_college.getSelectedItem().toString();
+                String major=auto_major.getText().toString();
                 if (isNumeric(et_age.getText().toString()))
                     age = Integer.parseInt(et_age.getText().toString());
-                String sex = sp_sex.getSelectedItem().toString();
-                if(!mandatory(name,major,age,sex)) return;
-                Student stu = new Student(name, major, age, sex);
+                if (!mandatory(name, major, age)) return;
+                Student stu = new Student(name,college,null,major,age,sex);
                 intent.putExtra("stu", stu);
-                intent.putExtra("position",position);
+                intent.putExtra("position", position);
                 setResult(0, intent);
                 finish();
             }
         });
     }
+
 
     /**
      * spinner 接收默认值的Spinner
@@ -74,8 +121,9 @@ public class EditActivity extends AppCompatActivity {
             }
         }
     }
-    public boolean mandatory(String name, String major, int age, String sex) {
-        if (name.equals(null) || name == ""|| name.length()<2) {
+
+    public boolean mandatory(String name, String major, int age) {
+        if (name.equals(null) || name == "" || name.length() < 2) {
             Toast.makeText(this, "请输入正确的姓名！", LENGTH_LONG).show();
             return false;
         }
@@ -87,14 +135,11 @@ public class EditActivity extends AppCompatActivity {
             Toast.makeText(this, "请输入正确的年龄！", LENGTH_LONG).show();
             return false;
         }
-        if (sex.equals("请选择性别")) {
-            Toast.makeText(this, "性别是必选项！", LENGTH_LONG).show();
-            return false;
-        }
         return true;
     }
+
     public static boolean isNumeric(String str) {
-        if(str.length()<2)
+        if (str.length() < 2)
             return false;
         for (int i = str.length(); --i >= 0; ) {
             if (!Character.isDigit(str.charAt(i))) {
--- ListViewApp/app/src/main/java/com/example/listviewapp/Student.java
+++ ListViewApp/app/src/main/java/com/example/listviewapp/Student.java
@@ -1,9 +1,46 @@
 package com.example.listviewapp;
 
+import android.animation.PropertyValuesHolder;
+
 import java.io.Serializable;
 
 
 public class Student implements Serializable {
+
+    private String name;
+    private String college;
+    private String[] courses;
+    private String major;
+
+    public Student(String name, String college, String[] courses, String major, int age, String sex) {
+        this.name = name;
+        this.college = college;
+        this.courses = courses;
+        this.major = major;
+        this.age = age;
+        this.sex = sex;
+    }
+
+    private int age;
+    private String sex;
+
+
+    public String getCollege() {
+        return college;
+    }
+
+    public void setCollege(String college) {
+        this.college = college;
+    }
+
+    public String[] getCourses() {
+        return courses;
+    }
+
+    public void setCourses(String[] courses) {
+        this.courses = courses;
+    }
+
     Student() {
         this.name = "Name";
         this.major = "Major";
@@ -14,9 +51,6 @@ public class Student implements Serializable {
         this.major = major;
     }
 
-    private String name;
-    private String major;
-
     public Student(String name, String major, int age, String sex) {
         this.name = name;
         this.major = major;
@@ -24,8 +58,6 @@ public class Student implements Serializable {
         this.sex = sex;
     }
 
-    private int age;
-    private String sex;
 
     public int getAge() {
         return age;
@@ -51,7 +83,9 @@ public class Student implements Serializable {
         this.major = major;
     }
 
-    public String getName() { return name; }
+    public String getName() {
+        return name;
+    }
 
     public String getMajor() {
         return major;
